2d305e0e63474780e917271e913c569cb87c965d
Malachi <m7524053@gmail.com>
2025-05-01 22:34:41 -0400

    Fix SQLite path for Render

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_dotenv()
 # Configuration
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
